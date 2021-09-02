a942741a6e40a290d82a8b7beeb8276777845789
HP <mega.pe4enka@gmail.com>
2021-09-03 00:35:22 +0300

    Ready Header

---
+++ components/Header/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Paper, Button, IconButton, Avatar } from '@material-ui/core';
+import {
+    SearchOutlined as SearchIcon,
+    SmsOutlined as MessageIcon,
+    Menu as MenuIcon,
+    ExpandMoreOutlined as ArrowBottom,
+    NotificationsNoneOutlined as NotificationIcon,
+    AccountCircleOutlined as UserIcon,
+    Create as PenIcon
+} from '@material-ui/icons';
+
+import styles from './Header.module.scss'
+
+const Header: React.FC = () => {
+    return (
+        <Paper classes={{ root: styles.root }} elevation={0}>
+            <div className='d-flex align-center'>
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <svg className={styles.logo} viewBox="0 0 24 25">
+                    <path fill="#e8a427" d="M0 19h8.5v6H0v-6z"></path>
+                    <path  d="M0 7h8.5v18l6.5-6V7h9V0H0v7z"></path>
+                    <path fill="rgba(0,0,0,0.15)" d="M7.5 19h1v6l-1-6z"></path>
+                </svg>
+
+                <div className={styles.searchBlock}>
+                    <SearchIcon />
+                    <input placeholder='Поиск'/>
+                </div>
+
+                <Button className={styles.penButton} variant='contained'>
+                    <PenIcon />
+                </Button>
+            </div>
+            <div className='d-flex align-center'>
+                <IconButton>
+                    <MessageIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationIcon />
+                </IconButton>
+                <Avatar className="ml-15" alt="Avatar" src=''/>
+                <Button>
+                    <ArrowBottom />
+                </Button>
+            </div>
+        </Paper>
+    );
+};
+
+export default Header;
