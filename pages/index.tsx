b2123157919f159f86416ec8c25e596057aa392d
HP <mega.pe4enka@gmail.com>
2021-09-03 11:52:29 +0300

    Update pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Header from "../components/Header";
+import Post from "../components/Post";
+import LeftMenu from "../components/LeftMenu";
 
 export default function Home() {
   return (
@@ -9,6 +11,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <div className="wrapper">
+          <div className='leftSide'>
+            <LeftMenu />
+          </div>
+          <div className='content'>
+            <Post />
+          </div>
+          <div className='rightSide'> </div>
+      </div>
     </div>
   )
 }
