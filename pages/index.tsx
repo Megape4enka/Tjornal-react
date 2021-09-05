d4eae5159647fee5e7f735cd8f7cb351399bec5e
HP <mega.pe4enka@gmail.com>
2021-09-05 19:47:41 +0300

    Added new component: CommentPost

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,16 @@
-import Head from 'next/head'
-import Header from "../components/Header";
-import Post from "../components/Post";
-import LeftMenu from "../components/LeftMenu";
+// @ts-ignore
+import { Post } from '../components/Post';
+import { MainLayout } from '../layouts/MainLayout';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Rjornal</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <div className="wrapper">
-          <div className='leftSide'>
-            <LeftMenu />
-          </div>
-          <div className='content'>
+    return (
+        <MainLayout>
             <Post />
-          </div>
-          <div className='rightSide'> </div>
-      </div>
-    </div>
-  )
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+        </MainLayout>
+    );
 }
